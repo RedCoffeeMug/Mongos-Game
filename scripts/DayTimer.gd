975bc546de6e6d296e431a6c039c6e3b6bfbfe8e
RedCoffeeMug <redcoffeemuggithub@gmail.com>
2023-08-11 21:09:01 -0400

    added mongo sprite & credits panel

--- scripts/DayTimer.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var day = true;
-signal time_changed()
-
-
-func _on_Timer_timeout():
-	day = not day
-	emit_signal("time_changed")
--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -1,6 +1,32 @@
 extends Control
 
-onready var popup = get_node("OptionsMenu")
+onready var CreditsPanel = get_node("CreditsPanel")
+
+
+func _ready():
+	#Generating text
+	var copyright_text = ""
+	
+	copyright_text += "Made by [url=https://github.com/RedCoffeeMug]RedCoffeeMug[/url]\n"
+	copyright_text += "Created using the [url=https://godotengine.org/license/]Godot Game Engine[/url]"
+	
+	#license
+	var copyright_info = Engine.get_copyright_info()
+	copyright_text += "\n\n===THIRD-PARTY COMPONETS===\n"
+	for part in copyright_info:
+		copyright_text += part["name"] + " - " + part["parts"][0]["license"] + "\n"
+	
+	#license text
+	copyright_text += "\n===LICENSE TEXTS===\n"
+	var licenses = Engine.get_license_info()
+	for license in licenses:
+		copyright_text += "-----" + license + "-----\n" + licenses[license] + "\n\n"
+	
+	#put text in label
+	$CreditsPanel/credits_label.bbcode_text = copyright_text
+	
+	CreditsPanel.hide()
+
 
 
 func _on_StartButton_pressed():
@@ -8,7 +34,7 @@ func _on_StartButton_pressed():
 
 
 func _on_OptionsButton_pressed():
-	popup.visible = true
+	pass
 
 
 func _on_QuitButton_pressed():
@@ -18,3 +44,10 @@ func _on_QuitButton_pressed():
 func _process(delta):
 	if $BackgroundMusic.playing == false:
 		$BackgroundMusic.play()
+
+
+func _on_CreditsButton_pressed():
+	if CreditsPanel.visible == false:
+		CreditsPanel.show()
+	else:
+		CreditsPanel.hide()
